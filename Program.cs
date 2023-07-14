9001399faf6cdb75a73014b944a67113b5dc2f2c
computeriest <gungorduelif@outlook.com>
2023-07-14 14:34:12 +0300

    Refactored

--- Program.cs
+++ Program.cs
@@ -28,49 +28,6 @@ public class HelloEvogens
 }
 
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Bir sayı girin: ");
-        int number = int.Parse(Console.ReadLine());
-
-        int sum = 0;
-        for (int i = 1; i <= number; i++)
-        {
-            sum += i;
-        }
-
-        Console.WriteLine("1'den " + number + " kadar olan sayıların toplamı: " + sum);
-
-    }
-}
-//class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Bir sayı girin: ");
-//            int number = int.Parse(Console.ReadLine());
-
-//            int sum = 0;
-//            int product = 1;
-
-//            for (int i = 1; i <= number; i++)
-//            {
-//                sum += i;
-//                product *= i;
-//            }
-
-//            Console.WriteLine("1'den " + number + " kadar olan sayıların toplamı: " + sum);
-//            Console.WriteLine("1'den " + number + " kadar olan sayıların çarpımı: " + product);
-
-//            // Diğer kodları buraya ekleyebilirsiniz
-//        }
-//    }
-
-//}
-
-
 
 
 
